367f90303b301429874e0d7a63410708a7b15267
gulfamkhan2308 <sohailahmadkhan2308@gmail.com>
2023-10-29 11:03:03 -0700

    updated

--- script.mjs
+++ script.mjs
@@ -1,10 +1,10 @@
+function marla(event){
+    console.log("button active");
+    event.preventDefault();
 
-document.getElementById('convertButton').addEventListener('click', () => {
-    const marlaInput = parseFloat(document.getElementById('marlaInput').value);
-    if (!isNaN(marlaInput)) {
-        const squareFeet = marlaInput * 272.25; // Assuming 1 marla is approximately 225 square feet
-        document.getElementById('result').textContent = `${marlaInput} marlas is equal to ${squareFeet} square feet.`;
-    } else {
-        document.getElementById('result').textContent = 'Please enter a valid number of marlas.';
-    }
-});
+    let inp= document.querySelector("#marlaInput").value;
+    let result= inp*272;
+    let massage= "area in square feet "+ result;
+    document.querySelector("#result").innerHTML= massage;
+
+}
